585bac20598727b4734168c86f305c715b2c168f
Gracekangdh <gracekangdh@gmail.com>
2024-07-23 21:39:13 +1000

    Set Current Date(Header)

--- src/components/DiaryHead/DiaryHead.jsx
+++ src/components/DiaryHead/DiaryHead.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./DiaryHead.module.css";
+import { getCurrentDate } from "../../Utils/Date";
 
 export default function DiaryHead() {
+  const { date, weekday } = getCurrentDate();
   return (
     <div className={styles.diaryHeadBlock}>
-      <h1>2019년 7월 10일</h1>
-      <div className={styles.day}>수요일</div>
+      <h1>{date}</h1>
+      <div className={styles.day}>{weekday}</div>
     </div>
   );
 }
