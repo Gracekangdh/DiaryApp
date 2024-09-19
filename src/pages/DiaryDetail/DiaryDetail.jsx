74afaf4cfa8f5f7730eaaa9a5ee36c97f2346b76
Gracekangdh <gracekangdh@gmail.com>
2024-09-19 20:16:47 +1000

    edit, save btn on detail page

--- src/pages/DiaryDetail/DiaryDetail.jsx
+++ src/pages/DiaryDetail/DiaryDetail.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDiaries } from "../../hooks/use-diaries";
 import styles from "../DiaryDetail/DiaryDetail.module.css";
+import { isEditable } from "@testing-library/user-event/dist/utils";
 
 export default function DiaryDetail() {
   const [diary, setDiary] = useState(null);
   const { id } = useParams();
   const navigate = useNavigate();
-  const { diaries, deleteDiary } = useDiaries();
+  const { diaries, deleteDiary, updateDiary } = useDiaries();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState("");
+  const [editedText, setEditedText] = useState("");
 
   const handleBackClick = () => {
     navigate(-1);
@@ -16,6 +20,21 @@ export default function DiaryDetail() {
   const handleDelete = () => {
     deleteDiary(id) //
       .then(() => navigate(-1)); // useEffect가 더 먼저 발생
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedTitle(diary.title);
+    setEditedText(diary.text);
+  };
+
+  const handleSave = async () => {
+    const updatedDiary = { ...diary, title: editedTitle, text: editedText };
+
+    await updateDiary(id, updatedDiary);
+
+    setDiary(updatedDiary);
+    setIsEditing(false);
   };
 
   useEffect(() => {
@@ -28,8 +47,28 @@ export default function DiaryDetail() {
   return (
     <div className={styles.cardDetail}>
       <button onClick={handleBackClick}>Back</button>
-      <h2>{diary.title}</h2>
-      <p>{diary.text}</p>
+
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+          <textarea
+            value={editedText}
+            onChange={(e) => setEditedText(e.target.value)}
+          />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          <h2>{diary.title}</h2>
+          <p>{diary.text}</p>
+          <button onClick={handleEdit}>Edit</button>
+        </>
+      )}
+
       <button onClick={handleDelete}>delete</button>
     </div>
   );
