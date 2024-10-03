2120610c15e9fbd68d98ef37b1ba1811a24f50f6
Gracekangdh <gracekangdh@gmail.com>
2024-10-03 21:20:27 +1000

    deleted onDelete prop

--- src/pages/DiaryList/DiaryList.jsx
+++ src/pages/DiaryList/DiaryList.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import DiaryCard from "../DiaryCard/DiaryCard";
 import styles from "../DiaryList/DiaryList.module.css";
 
-export default function DiaryList({ diaries, onDelete }) {
+export default function DiaryList({ diaries }) {
   return (
     <ul>
       {diaries.map((diary) => (
         <li key={diary.id} className={styles.list}>
-          <DiaryCard diary={diary} onDelete={onDelete} />
+          <DiaryCard diary={diary} />
         </li>
       ))}
     </ul>
